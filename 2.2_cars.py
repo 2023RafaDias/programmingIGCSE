4c8188da3988ae5232ab5e97e1d31fd7d0b74ca2
2023RafaDias <2023rafaeladias@gmail.com>
2021-04-16 12:48:17 -0300

    Car Gallon program
    Calculates how many gallons to fill a car's tank

---
+++ 2.2_cars.py
@@ -0,0 +1,41 @@
+miles = []
+gallons = 0
+distance = 0
+need = []
+amount = 0
+count = -1
+more = "yes"
+correct = ""
+while more == "yes":
+    while correct != "yes":
+        try:
+            miles.append(float(input("Enter the mileage of the car the last time it was filled: ")))
+        except ValueError:
+            print("integer only!")
+
+        try:
+            miles.append(float(input("Enter the mileage of the car now that the tank is empty: ")))
+        except ValueError:
+            print("integer only!")
+        
+        distance = miles[count+2] - miles[count+1]
+        if distance < 1:
+            miles.pop(count+2)
+            miles.pop(count+1)
+            print("Invalid options as the amount of miles can't have decreased. Verify for error in your car's system")
+        else:
+            correct = "yes"
+    try:
+        litres = float(input("Litres to fill the tank: "))
+    except ValueError:
+        print("integer only!")
+
+    gallons = litres*0.26
+    need.append(gallons/distance)
+    print("The amount of gallons this car needs of is ", need[amount])
+    amount += 1
+    count+=2
+    correct = ""
+    more = input("Is there another car you would like to verify?")
+for i in range (0,amount):
+    print("Car ",i+1," requires ",need[i]," gallons.")
