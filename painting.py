b7b1aab860104602f20d7f807adc8c4b59b161d7
2023RafaDias <2023rafaeladias@gmail.com>
2021-04-20 23:10:05 -0300

    solved problem of Objects()

--- painting.py
+++ painting.py
@@ -1,27 +1,28 @@
 room = [['length','height','width','radius','circumference',"room's","object's"],[0,0,0,0],[0,0,0,0]]
+amount = [0]
 
-def Objects():
-    amount = 0
-    quantity = 0
-    error = "yes"
-    while error == "yes":
+def Objects(amount2):
+    amount = amount2
+    error = "no"
+    while error == "no":
         try:
-            quantity = int(input("How many objects of this type are there?"))
-            error = "no"
+            quantity = float(input("How many objects of this type are there? "))
+            error = "yes"
         except ValueError:
-            print("Float Only!")
-    amount= room[2][3]
-    for i in range(1,quantity):
-        room[2][3]+= amount
-    amount = amount*quantity
-    if room[2][3]> room[1][3]:
-        
-        print("Area unpainted is larger than area painted. INVALID objects")
-        room[2][3]-= amount
+            print("Error!Float only!")
+
+        amount2*=quantity
+    
+        room[2][3] += amount2
+        room[2][3] -= amount
+        if room[2][3] > room[1][3]:
+            print("Size of unpainted objects is larger then room. Objects rejected")
+            room[2][3] -= amount2
+
 
 def Calculate (value,value2,a,y):
     room[y][3]+= value*value2*a
-    amount = value*value2*a
+    amount[0] = value*value2*a
 
 def CalculateRegular (x,z,y):
     for i in range (0,x):
@@ -39,8 +40,8 @@ def CalculateRegular (x,z,y):
         Calculate(room[y][m],room[y][1],x-1,y)
         m = 2
 
+finalUnpainted = 0
 CalculateRegular(3,5,1)
-print(room[1][3])
 answer = input("Are there any unpaintable objects? Use Y/N")
 while answer == "Y":
     shape = ""
@@ -54,16 +55,18 @@ while answer == "Y":
                 error = "yes"
             except ValueError:
                 print("Float Only!")
-
         room[2][3]+= radius*radius*3.1415
-        amount = radius*radius*3.1415
-        Objects()
+        amount2 = radius*radius*3.1415
+        Objects(amount2)
     else:
         CalculateRegular(2,6,2)
-        Objects()
+        Objects(amount[0])
     answer = input("Another object? Use Y/N")
 
 totalArea = room[1][3] - room[2][3]
+print("final values of both")
+print(room[1][3])
+print(room[2][3])
 print(totalArea)
 
 error = "yes"
@@ -76,7 +79,4 @@ while error == "yes":
 
 totalArea *= multiply
 gallons = totalArea/400
-print("You will need, ",gallons, "gallons.")
-
-#LABEL OBJECTS!
-#STORE OBJECTS THAT WERE INVALIDATED. 
+print("You will need ",gallons, "gallons of paint.")
