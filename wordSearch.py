ba3ed7f3d16969049120603dfb9ad76de95c92af
2023RafaDias <2023rafaeladias@gmail.com>
2021-04-21 11:32:39 -0300

    not case sensitive anymore

--- wordSearch.py
+++ wordSearch.py
@@ -12,7 +12,7 @@ while repeat == "Y":
         string = name.split(" ")
         size = len(string)
         firstname,surname = string[0], string[size-1]
-        print(firstname[0],surname)
+        print(surname,firstname[0])
         print("Firstname is ",firstname)
         amount = len(firstname)
         for i in range(amount-1,-1,-1):
@@ -21,17 +21,19 @@ while repeat == "Y":
                 if vowels[x] in firstname[i]:
                     vowel+=1
         print("Amount of vowels is ", vowel)
-        print("Reversed is ", firstnameRev)
+        print("Reversed is ", firstnameRev.upper())
         again = "Y"
         while again == "Y":
-            search = input("Search for a specific letter(notice the program is case sensitive: ")
+            search = input("Search for a specific letter: ")
             if len(search) > 1:
                 print("please use only one character")
             else:
                 searchRes = 0
                 for i in range(amount-1,-1,-1):
-                    if search in firstname[i]:
+                    if search.lower() in firstname[i]:
                         searchRes += 1
+                    if search.upper() in firstname[i]:
+                        searchRes+= 1
                 print("amount: ",searchRes)
             again = input("Another search? Use Y/N ")
     repeat = input("Again? Use Y/N")
