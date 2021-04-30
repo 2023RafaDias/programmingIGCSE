e3fbbaae781a1a5947e448e9a2d7cdc9356c772e
2023RafaDias <2023rafaeladias@gmail.com>
2021-04-30 11:54:54 -0300

    truly fixed

--- vowelConsonant.py
+++ vowelConsonant.py
@@ -4,29 +4,27 @@ consonantNum = 0
 vowelInclude = ""
 consonantInclude = ""
 sentence = input("Please enter your word/sentence here: ")
+sentence = sentence.replace(" ","")
 for i in range(0,len(sentence)):
-    print(i)
     for x in range(0,len(vowels)):
-        print(x)
         include = ""
         if vowels[x] in sentence[i]:
             vowelInclude += sentence[i]
             vowelNum += 1
-            print("yes",sentence[i],vowels[x])
             include = "no"
+            break
         elif vowels[x].upper() in sentence[i]:
             vowelInclude += sentence[i]
             vowelNum += 1
-            print("yes upper",sentence[i],vowels[x])
-            include= "no"
-        if include == "":
+            include = "no"
+            break
+        elif include != "no" and x == 4:
             consonantInclude += sentence[i]
             consonantNum += 1
-            print("no",sentence[i],vowels[x])
 
 
-print(vowelNum)
-print(consonantNum)
-print(vowelInclude)
-print(consonantInclude)
+print("The number of vowels is:",vowelNum)
+print("The number of consonants is:",consonantNum)
+print("Only the vowels are:",vowelInclude)
+print("Only the consonants are:",consonantInclude)
 
