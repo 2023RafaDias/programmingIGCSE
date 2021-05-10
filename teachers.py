6ba478e0ef1a7b6588c755db309644a40bfe8d81
2023RafaDias <2023rafaeladias@gmail.com>
2021-05-09 21:31:25 -0300

    update teachers.py

--- teachers.py
+++ teachers.py
@@ -1,6 +1,13 @@
 classHold = []
 average = []
-size = int(input("How many students do you have? "))
+error = "yes"
+while error == "yes":
+    try:
+        size = int(input("How many students do you have? "))
+        break
+    except ValueError:
+        print("Integer Only!")
+
 count = 1
 while len(classHold) != size:
     print("Enter Student ",count,"'s name here: ")
@@ -14,8 +21,20 @@ def Test(number):
     average[number-1].append(0)
     average[number-1].append(0)
     for i in range (0,len(classHold)):
-        print("Enter the test score for test ", number, " here (out of ten) : ")
-        score = int(input())
+        score = -1
+        while score > 10 or score < 0:
+            error = "no"
+            while error == "no":
+                try:
+                    print("Enter the test score for test ", number, " here (out of ten) : ")
+                    score = int(input())
+                    error = "yes"
+                except ValueError:
+                    print("Invalid! Integer Only!")
+                    
+            if score > 10 or score < 0:
+                print("Invalid! Enter value between 0 and 10")
+        
         average[number-1][0] += score
         classHold[i][1].append(score)
     average[number-1][1] =(average[number-1][0]/len(classHold))
@@ -36,20 +55,18 @@ for i in range(0,size):
     for ii in range(0,loop):
         add = classHold[i][1][ii]
         amount += int(add)
-        classHold[i][1][size] = amount/loop
-    print("Student ",classHold[i][0],"'s avg grade of the year was of ",classHold[i][1][size])
-    if classHold[i][1][size] <= 6:
+        classHold[i][1][-1] = amount/loop
+    print("Student ",classHold[i][0],"'s avg grade of the year was of ",classHold[i][1][-1])
+    if classHold[i][1][-1] < 5:
         classHold[i][1].append("not pass")
-        print("Student ",classHold[i][0],"did ",classHold[i][1][len(classHold)+1])
+        print("Student ",classHold[i][0],"did ",classHold[i][1][-1])
         for ii in range(0,loop):
             if classHold[i][1][ii] < 5:
                 print("Test ",ii+1,"needs to be retaken by ",classHold[i][0])
     else:
         classHold[i][1].append("pass")
-        print("Student ",classHold[i][0],"did ",classHold[i][1][len(classHold)+1])
+        print("Student ",classHold[i][0],"did ",classHold[i][1][-1])
         for ii in range(0,loop):
             if classHold[i][1][ii] < 5:
                 print("However, Test ",ii+1,"needs to be retaken by ",classHold[i][0])
 
-from tabulate import tabulate
-print(tabulate(classHold[0], classHold[1], headers=['Name', 'Age']))
