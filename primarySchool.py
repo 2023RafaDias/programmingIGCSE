9d91dc4a743faf425561dd013b0bfc0e5c50cbd5
2023RafaDias <2023rafaeladias@gmail.com>
2021-06-02 12:34:32 -0300

    updated to add a multiplication challenge

--- primarySchool.py
+++ primarySchool.py
@@ -1,3 +1,6 @@
+import time
+import random
+
 while True:
     name = input("What is your name? ")
     base = int(input("What is the base? "))
@@ -15,3 +18,43 @@ while True:
     again = input("again? Use Y: ")
     if again != "Y":
         break
+
+def GameMode(base1,base2,score):
+    print(base1,"x",base2,"=",end = " ")
+    answer = int(input("enter here:"))
+    if answer == base1*base2:
+        score += 1
+        print("WELL DONE! Your answer is CORRECT!")
+    else:
+        print("Oh No! Your answer is wrong...")
+        print("Correct answer = ",base1*base2)
+    return score
+
+def Base(level):
+    if level == "E":
+        base = random.randint(0,10)
+    elif level == "M":
+        base = random.randint(2,20)
+    elif level == "H":
+        base = random.randint(2,100)
+    return base
+    
+game = input("Would you like to enter game mode?")
+while game == "Y":
+    score = 0
+    level = input("What difficulty? Easy/Medium/Hard E/M/H: ")
+    quest = int(input("Enter the number of questions: "))
+
+        
+    print("Game starting in....")
+    for i in range(3,0,-1):
+        print(i,"...")
+        time.sleep(1)
+    print("GO!")
+    for i in range(0,quest):
+        base1 = Base(level)
+        base2 = Base(level)
+        score = GameMode(base1,base2,score)
+    print("THE END")
+    print("Your final score was ",score,"/",quest,"\nor ",score/quest*100,"%")
+    game = input("Again? Use Y: ")
