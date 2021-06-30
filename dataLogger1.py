8734a1068ebe977dd75b9b25bf8ffef74334ae1b
2023RafaDias <2023rafaeladias@gmail.com>
2021-06-30 11:48:18 -0300

    first version

--- dataLogger1.py
+++ dataLogger1.py
@@ -2,7 +2,19 @@ time = ["midday","midnight"]
 dataLogger = []
 def NewTemp(i):
     print("Temp for ",time[i])
-    temp = float(input("Enter here: "))
+    while True:
+        try:
+            temp = float(input("Enter here: "))
+            if temp > 60:
+                print("Invalid Temperature. Too Hot")
+            elif temp < -60:
+                print("Invalid Temperature. Too Cold")
+            else:
+                break
+
+        except ValueError:
+            print("Invalid Temperature. Enter float number only.\nTry Again.")
+    
     dataLogger[-1].append(temp)
 
 for i in range(0,30):
---
+++ pre-release.py
@@ -0,0 +1,55 @@
+import random
+courts = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]]
+
+#[code,"name","phone"]
+guests = []
+
+hours = [8,9,10,11,12,13,14,15,16,17,18]
+
+"""def Free(number):
+    free = 0
+    for i in range (0,8):
+        if not(courts[number][i]):
+            free += 1
+    courts[number][8] = free
+    return free"""
+
+def CheckTime(selected):
+    store = 0
+    for i in range(0,10):
+        if selected == hours[i]:
+            store = i
+            print(store)
+    for i in range(0,8):
+        print("Court: ",i+1)
+        if not(courts[i][store]):
+            print("Free")
+
+def Availability(i,j):
+    if not(courts[i][j]):
+        print("Free")
+    else:
+        print("booked")
+
+for i in range(0,8):
+    print("Court ",i+1,"'s availability: ")
+    print(" ")
+    for j in range(0,10):
+        print("Hour: ",hours[j],":00 to",(hours[j]+1),":00")
+        Availability(i,j)
+    print(" ")
+
+def Schedule(court,time):
+    guests.append([])
+    guests[-1].append(random.randint(1000,9999))
+    name = input("What is the guest's name?")
+    guests[-1].append(name)
+    phone = input("What is the guest's phone number?")
+    guests[-1].append(phone)
+    courts[court][time] = len(guests)
+
+Schedule(7,3)
+print(courts[7])
+print(guests)
+
+
