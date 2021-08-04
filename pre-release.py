dfcd0fbcdbc092919dd23da57aa28050c734c63e
2023RafaDias <2023rafaeladias@gmail.com>
2021-08-04 12:32:01 -0300

    removed unnecessary functions

--- pre-release.py
+++ pre-release.py
@@ -1,6 +1,5 @@
 import random
 
-#stores the guest position + 1 or 0 if it is free
 courts = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]]
 
 corresponding = ["code","name","phone"]
@@ -18,13 +17,11 @@ day = "started"
 print("Day Has Started")
 print(" ")
 
-#finds the time position correspondant to user's entry
 def timeArray(selected):
     for i in range(0,10):
         if selected == hours[i]:
             return int(i)
 
-#checks if a specific court is free at a specific time
 def Availability(i,j):
     if not(courts[i][j]):
         print("Free")
@@ -34,27 +31,16 @@ def Availability(i,j):
         for i in range(0,3):
             print(corresponding[i],": ",guests[position][i])
 
-#checks what courts are free at a specific time
-def CheckTime(selected):
-    store = 0
-    store = timeArray(selected)
-    for i in range(0,8):
-        print("Court: ",i+1)
-        Availability(i,store)
+def Schedule(court,time):
+    guests.append([])                          
+    guests[-1].append(1890)
 
-#makes sure that all codes are unique
-def CheckUnique():
     rep = len(guests)
     if rep > 1:
         for i in range(0,rep-1):    
             while guests[-1][0] == guests[i][0]:
                 guests[-1][0] = random.randint(1000,9999)
-
-#sets up a new guest with info
-def Schedule(court,time):
-    guests.append([])                          
-    guests[-1].append(1890)
-    CheckUnique()
+    
     name = input("What is the guest's name?")
     guests[-1].append(name)
     correct = "NO"
@@ -65,7 +51,6 @@ def Schedule(court,time):
     guests[-1].append(phone)
     courts[court][time] = len(guests)
 
-#schedules the guest and makes sure it is valid
 def CourtSchedule():
     timeOption = int(input("At what time would you like to book?"))
     time = timeArray(timeOption)
@@ -119,7 +104,12 @@ while day != "Y":
             x = CourtSchedule()
     elif choice == 2:
         check = int(input("Enter the starting hour of the time you want to check: "))
-        CheckTime(check)
+        store = 0
+        store = timeArray(check)
+        for i in range(0,8):
+            print("Court: ",i+1)
+            Availability(i,store)
+        
     elif choice == 3:
         Action3()
         
@@ -143,11 +133,9 @@ for j in range(0,2):
     else:
         print(names[j],": None, There were no bookings")
         
-
 print("")
 
 for i in range(0,8):
     totalNumber += amount[1][i]
-
 
 print("Total number of bookings was ",totalNumber)
