8dffe937958eafc25309d6b7963b9c508f22b132
2023RafaDias <2023rafaeladias@gmail.com>
2021-09-06 16:47:19 -0300

    finished data Logger

--- dataLogger1.py
+++ dataLogger1.py
@@ -1,5 +1,13 @@
 time = ["midday","midnight"]
 dataLogger = []
+dataLogger.append([[]])
+dataLogger[-1].append([])
+total = [0,0]
+dayMax = 0
+nightLow = 0
+
+print(dataLogger)
+
 def NewTemp(i):
     print("Temp for ",time[i])
     while True:
@@ -14,11 +22,26 @@ def NewTemp(i):
 
         except ValueError:
             print("Invalid Temperature. Enter float number only.\nTry Again.")
+
+    dataLogger[-1][i].append(temp)
     
-    dataLogger[-1].append(temp)
 
 for i in range(0,30):
     print("Day",i+1)
-    dataLogger.append([])
-    for i in range(0,2):
-        NewTemp(i)
+    for j in range(0,2):
+        NewTemp(j)
+        total[j] += dataLogger[-1][j][-1]
+    if i >1:
+        if dataLogger[-1][0][-1]>dataLogger[-1][0][-2]:
+            dayMax = dataLogger[-1][0][-1]
+        if dataLogger[-1][1][-1]<dataLogger[-1][1][-2]:
+            nightLow = dataLogger[-1][1][-1]
+
+print("The Average Midday Temperature was of ",total[0]/30,"°C")
+print("The Average Midnight Temperature was of ",total[1]/30,"°C")
+print(dayMax)
+print(nightLow)
+
+
+
+print(dataLogger)
