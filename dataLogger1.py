59afccf7bee5c30ddece1a5a49173af451cef52a
2023RafaDias <2023rafaeladias@gmail.com>
2021-09-15 11:59:07 -0300

    updated

--- dataLogger1.py
+++ dataLogger1.py
@@ -1,47 +1,46 @@
 time = ["midday","midnight"]
 dataLogger = []
-dataLogger.append([[]])
-dataLogger[-1].append([])
-total = [0,0]
-dayMax = 0
-nightLow = 0
+answer = "Y"
 
-print(dataLogger)
+while answer == "Y":
+    dataLogger.append([[]])
+    dataLogger[-1].append([])
+    total = [0,0]
+    dayMax = -60
+    nightMin = 60
 
-def NewTemp(i):
-    print("Temp for ",time[i])
-    while True:
-        try:
-            temp = float(input("Enter here: "))
-            if temp > 60:
-                print("Invalid Temperature. Too Hot")
-            elif temp < -60:
-                print("Invalid Temperature. Too Cold")
-            else:
-                break
+    def NewTemp(i):
+        print("Temp for ",time[i])
+        while True:
+            try:
+                temp = float(input("Enter here: "))
+                if temp > 60:
+                    print("Invalid Temperature. Too Hot")
+                elif temp < -60:
+                    print("Invalid Temperature. Too Cold")
+                else:
+                    break
 
-        except ValueError:
-            print("Invalid Temperature. Enter float number only.\nTry Again.")
+            except ValueError:
+                print("Invalid Temperature. Enter float number only.\nTry Again.")
 
-    dataLogger[-1][i].append(temp)
-    
+        dataLogger[-1][i].append(temp)
+        
 
-for i in range(0,30):
-    print("Day",i+1)
-    for j in range(0,2):
-        NewTemp(j)
-        total[j] += dataLogger[-1][j][-1]
-    if i >1:
-        if dataLogger[-1][0][-1]>dataLogger[-1][0][-2]:
-            dayMax = dataLogger[-1][0][-1]
-        if dataLogger[-1][1][-1]<dataLogger[-1][1][-2]:
-            nightLow = dataLogger[-1][1][-1]
+    for i in range(0,30):
+        print("Day",i+1)
+        for j in range(0,2):
+            NewTemp(j)
+            total[j] += dataLogger[-1][j][-1]
+        if i >1:
+            if dataLogger[-1][0][-1]>dayMax:
+                dayMax = dataLogger[-1][0][-1]
+            if dataLogger[-1][1][-1]<nightMin:
+                nightMin = dataLogger[-1][1][-1]
 
-print("The Average Midday Temperature was of ",total[0]/30,"°C")
-print("The Average Midnight Temperature was of ",total[1]/30,"°C")
-print(dayMax)
-print(nightLow)
+    print("The Average Midday Temperature was of ",round(total[0]/30,2),"°C")
+    print("The Average Midnight Temperature was of ",round(total[1]/30,2),"°C")
+    print("The maximum midday temperature was ",dayMax)
+    print("The minimum midnight temperature was ",nightMin)
 
-
-
-print(dataLogger)
+    answer = input("Next Month? Y/N: ")
